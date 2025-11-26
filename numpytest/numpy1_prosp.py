25dee1b5e9b7a0ffd1d79440fb81fca132933e72
collapsegod <pr@mail.ru>
2025-11-26 20:22:40 +0300

    dz1

---
+++ numpytest/numpy1_prosp.py
@@ -0,0 +1,7 @@
+import numpy as np
+a= np.array([5, 2, 8, 1, 9, 3])
+print(a)
+print(a.ndim)
+print(a.shape)
+print(a.dtype)
+print(a.size)
---
+++ numpytest/numpy2_prosp.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr1 = np.arange(15, 31)
+print(arr1)
+arr2 = np.linspace(0, 1, 10) 
+print(arr2)
